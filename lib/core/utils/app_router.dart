93197aaaa61305358e1e464238755cabec84993d
Mohamed Ashraf <medoalex350@gmail.com>
2024-11-30 22:34:27 +0200

    add app router

--- lib/core/utils/app_router.dart
+++ lib/core/utils/app_router.dart
@@ -1,32 +1,24 @@
 
 
+import 'package:attendy/features/splash/presentation/views/splash_view.dart';
+import 'package:go_router/go_router.dart';
+
 abstract class AppRouter {
   // example uncomment and use
 
   // static const kHomeView = '/homeView';
   // static const kBookDetailsView = '/bookDetailsView';
   // static const kSearchView = '/searchView';
-  // static final router = GoRouter(
-  //   routes: [
-  //     GoRoute(
-  //       path: '/',
-  //       builder: (context, state) => const SplashView(),
-  //     ),
+   static final router = GoRouter(
+     routes: [
+       GoRoute(
+         path: '/',
+         builder: (context, state) => const SplashView(),
+       ),
   //     GoRoute(
   //       path: kHomeView,
   //       builder: (context, state) => const HomeView(),
   //     ),
-  //     GoRoute(
-  //       path: kBookDetailsView,
-  //       builder: (context, state) => BlocProvider(
-  //         create: (context) => SimilarBooksCubit(getIt.get<HomeRepoImpl>()),
-  //         child: BookDetailsView(bookModel: state.extra as BookModel,),
-  //       ),
-  //     ),
-  //     GoRoute(
-  //       path: kSearchView,
-  //       builder: (context, state) => const SearchView(),
-  //     ),
-  //   ],
-  // );
+     ],
+  );
 }
